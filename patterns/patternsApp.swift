bf5050320291eea7628edf64be71f3c8a2cc9d5b
Nickolay Trukhin <coolone.official@gmail.com>
2021-11-02 22:07:57 +0300

    Initial Commit

---
+++ patterns/patternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  patternsApp.swift
+//  patterns
+//
+//  Created by Nickolay Truhin on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct patternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
